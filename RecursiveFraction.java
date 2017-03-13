2e8b5b82fecd6dc8afb18ce02be3f4205eb973ad
jevgenieva <jevgenieva@gmail.com>
2017-03-13 20:38:02 +0200

    recursively find the sum of all fractions from 1/2 to 1/2(to the power of an input number)

---
+++ RecursiveFraction.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RecursiveFraction {
+
+	public static void main(String[] args) {
+		//print the sum of all the fractions from 1/2 to 1/"2 to the power of an input number"
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a number: ");
+		int pow = sc.nextInt();
+		
+		System.out.println(recursiveFraction(0, pow, 1));
+		sc.close();
+		
+	}
+	static double recursiveFraction(double sum, int pow, double fract) {
+		if(pow <= 0)
+			return sum;
+		
+		fract *= 2;
+		sum += 1/fract;
+		
+		return recursiveFraction(sum, --pow, fract);
+	}
+}
